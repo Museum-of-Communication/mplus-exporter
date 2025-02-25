cd307b95369d72b5b29dfde31ea699104f0f510c
Nicolas Kessler <mail@nicolaskessler.ch>
2025-02-26 00:01:29 +0100

    export images to s3

---
+++ img_export.py
@@ -0,0 +1,194 @@
+import os
+import csv
+import io
+from dotenv import load_dotenv
+from datetime import datetime
+from s3client import S3Client
+from mpluspy import MPlusClient
+
+load_dotenv()
+
+
+class ImgExporter:
+
+    TIMESTAMP_FORMAT = "%Y-%m-%d %H:%M:%S"
+    LAST_RUN_KEY = "last-run.txt"
+    PENDING_KEY = "pending.csv"
+    CSV_HEADER = ["ID", "time-added"]
+
+    def __init__(self):
+        self.s3 = S3Client(
+            "config/s3.yml", os.getenv("s3-access-key"), os.getenv("s3-secret-key")
+        )
+        mplus_auth = (os.getenv("mplus-user"), os.getenv("mplus-pass"))
+        self.mplus = MPlusClient("config/mplus.yml", auth=mplus_auth)
+
+    def run_export(self):
+        # setup export and get state of last run
+        self._setup_export()
+
+        # query new images and add them to pending list
+        new = self._query_for_new_images(self.last)
+        self._append_new_images(new if new else [])
+
+        # save pending and last run before downloading images
+        self._save_pending()
+        self._save_last_run()
+
+        # download images and save them to s3
+        self._process_pending()
+
+        # save processed pending
+        self._save_pending()
+
+    def _setup_export(self):
+        """Gets and sets up state of last export."""
+        self.NOW = datetime.now()
+        self.last = self._get_last_run()
+        self.pending = self._load_pending()
+
+    def _get_last_run(self):
+        """Retreive and parse last run from file on S3. If none is found current time will be set as last run."""
+        key = self.LAST_RUN_KEY
+        if not self.s3.check_key(key):
+            Logger.log(
+                f"No last run file was found ({key}) on S3. Current time will be set as Last run and saved to S3 now. Edit {key} afterwards to set te correct timestamp!",
+                "WARNING",
+            )
+            return self.NOW
+
+        s = self.s3.get_object_string(key)
+        return datetime.strptime(s, self.TIMESTAMP_FORMAT)
+
+    def _save_last_run(self):
+        """Saves timestamp of last successfull query as txt to S3"""
+        self.s3.put_object(
+            key=self.LAST_RUN_KEY,
+            object=self.NOW.strftime(self.TIMESTAMP_FORMAT),
+            content_type="text/pain",
+        )
+
+    def _load_pending(self):
+        """Load list of pending images from csv on S3. If the file is not found the list will be initialized as empty"""
+        key = self.PENDING_KEY
+        if not self.s3.check_key(key):
+            Logger.log(
+                f"No pending file found ({key}) on S3. Initializing empty list.",
+                "WARNING",
+            )
+            return []
+
+        s = self.s3.get_object_string(key)
+        return list(csv.DictReader(io.StringIO(s)))
+
+    def _save_pending(self):
+        """Saves list of pending images as csv to S3"""
+        csv_buffer = io.StringIO()
+        writer = csv.DictWriter(csv_buffer, fieldnames=self.CSV_HEADER)
+        writer.writeheader()
+        writer.writerows(self.pending)
+        self.s3.put_object(
+            key=self.PENDING_KEY, object=csv_buffer.getvalue(), content_type="text/csv"
+        )
+
+    def _query_for_new_images(self, last_run):
+        """Query Mplus for Digital Assets modifed after given date. Returns list of IDs"""
+        Logger.log(f"Get IDs of all digital assets in MPlus modified since {last_run}.")
+        timestamp = MPlusClient.format_timestamp(last_run)
+        response = self.mplus.request(
+            "image-search", xml_placeholders={"timestamp": timestamp}
+        )
+        return response.parse_IDs()
+
+    def _append_new_images(self, ids):
+        """Adds list of IDs to pending images"""
+        Logger.log(f"Adding {len(ids)} new entries to list of pending assets.")
+        self.pending.extend(
+            dict(zip(self.CSV_HEADER, [item, self.NOW])) for item in ids
+        )
+
+    def _process_pending(self):
+        """Iterates over each entry of list of currently pending images.
+        If an entry already exists on S3 it is skipped and removed from pending.
+        If not the image is downloaded from the MuseumPlus API and saved to S3.
+        """
+        Logger.log("Processing pending images")
+        aborted = False
+        done = []
+
+        for item in self.pending:
+            try:
+                id_ = item["ID"]
+                key = self._id_to_key(id_)
+
+                if self.s3.check_key(key):
+                    Logger.log(
+                        f"{key} already exists on S3. Removing it from pending..."
+                    )
+
+                else:
+                    Logger.log(f"Downloading {key}")
+                    self._download_and_save_image(id_)
+
+                done.append(item)
+
+            except KeyboardInterrupt:
+                # Terminate downloading if user sends keyboard interrupt
+                Logger.log("KeyboardInterrupt: Execution was halted", "ERROR")
+                aborted = True
+                break
+
+            except Exception as e:
+                # Skip image if error occurs
+                Logger.log(
+                    f"Processing of {key} failed: {type(e).__name__}\nError Message: {e}",
+                    "ERROR",
+                )
+                aborted = True
+
+        self.pending = [item for item in self.pending if item not in done]
+
+        Logger.log(
+            (
+                "Some errors occurred. Pending images might still exist. Re-run the script to complete the process."
+                if aborted
+                else "Querying and downloading of all images finished successfully!"
+            ),
+            "WARNING" if aborted else "SUCCESS",
+        )
+
+    def _download_and_save_image(self, id_):
+        self.s3.put_object(
+            key=self._id_to_key(id_),
+            object=self.mplus.request("image-download", url_placeholders={"id": id_}).content,
+            content_type="image/jpeg",
+        )
+
+    def _id_to_key(self, id_):
+        return f"{id_}.jpg"
+
+
+class Logger:
+    COLORS = {
+        "INFO": "\033[39m",  # White/Default
+        "WARNING": "\033[93m",  # Yellow
+        "ERROR": "\033[91m",  # Red
+        "SUCCESS": "\033[92m",  # Green
+        "RESET": "\033[0m",  # Reset color
+    }
+
+    @staticmethod
+    def log(text: str, type: str = "RESET"):
+        """Prints text in a color depending on the type."""
+        color = Logger.COLORS.get(type.upper(), Logger.COLORS["RESET"])
+        timestamp = datetime.now()
+        print(f"{timestamp} ===> {color}{text}{Logger.COLORS['RESET']}")
+
+
+def main():
+    exporter = ImgExporter()
+    exporter.run_export()
+
+
+if __name__ == "__main__":
+    main()
--- s3client.py
+++ s3client.py
@@ -1,56 +1,49 @@
 import boto3
 import botocore
+import yaml
 
 
 class S3Client:
     """Wrapper for boto3"""
 
-    def __init__(
-        self,
-        region_name,
-        endpoint_url,
-        bucket_name,
-        access_key,
-        secret_key,
-        key_prefix="",
-    ):
+    def __init__(self, config_file, access_key, secret_key):
+        with open(config_file, "r") as f:
+            S3_CONFIG = yaml.load(f, Loader=yaml.FullLoader)
+
         session = boto3.session.Session()
         self.client = session.client(
             "s3",
-            region_name=region_name,
-            endpoint_url=endpoint_url,
+            region_name=S3_CONFIG["region-name"],
+            endpoint_url=S3_CONFIG["endpoint-url"],
             aws_access_key_id=access_key,
             aws_secret_access_key=secret_key,
         )
-        self.BUCKET = bucket_name
-        self.KEY_PREFIX = key_prefix
+        self.BUCKET = S3_CONFIG["bucket-name"]
+        self.KEY_PREFIX = S3_CONFIG["key-prefix"]
 
-    def checkKey(self, key: str) -> bool:
+    def check_key(self, key: str) -> bool:
         """Returns True if and only if a given key was found by client. Any exceptions result in False"""
-
         try:
-            response = self.client.head_object(
-                Bucket=self.BUCKET, Key=self.__prefixKey(key)
-            )
-            if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
-                return True
+            self.client.head_object(Bucket=self.BUCKET, Key=self.__prefix_key(key))
+            return True
         except botocore.exceptions.ClientError:
             return False
-        return False
 
-    def putObject(self, key: str, object, contentType: str):
+    def put_object(self, key: str, object, content_type: str):
         """Saves object as bytes with key to S3"""
         self.client.put_object(
             Bucket=self.BUCKET,
-            Key=self.__prefixKey(key),
+            Key=self.__prefix_key(key),
             Body=object,
-            ContentType=contentType,
+            ContentType=content_type,
         )
 
-    def getObjectString(self, key: str) -> str:
+    def get_object_string(self, key: str) -> str:
         """Returns object by key decoded as a string"""
-        response = self.client.get_object(Bucket=self.BUCKET, Key=self.__prefixKey(key))
+        response = self.client.get_object(
+            Bucket=self.BUCKET, Key=self.__prefix_key(key)
+        )
         return response["Body"].read().decode("utf-8")  # Decode bytes to string
 
-    def __prefixKey(self, key: str) -> str:
+    def __prefix_key(self, key: str) -> str:
         return f"{self.KEY_PREFIX}/{key}"
